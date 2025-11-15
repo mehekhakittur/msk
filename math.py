66540da25929acdfa2ee64d598284ce973b5e482
veereshbetageri1718 <betageriveeresh17@gmail.com>
2025-11-15 09:01:48 +0530

    modify

--- math.py
+++ math.py
@@ -3,5 +3,10 @@ try:
   b=float(input("Enter second number:"))
   print(f"Addition:{a+b}")
   print(f"subtraction:{a-b}")
+  print(f"MUltiplication:{a*b}")
+  if b!=0:
+    print(f"Division:{a/b}")
+  else:
+    print("Division by zero is not allowed.")
 except ValueError:
   print("please enter valid numbers.")
